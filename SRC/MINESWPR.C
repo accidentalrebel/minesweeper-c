6ca49d8824626b11d371e8d63c068e106e8d5a22
Juan Karlo Licudine <accidentalrebel@gmail.com>
2018-08-01 18:16:11 +0800

    Now using the correct newline endings.

--- SRC/MINESWPR.C
+++ SRC/MINESWPR.C
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <dos.h>
 
-void test()
-{
+void test(){
   union REGS in, out;
-
   in.h.ah = 0x2;
   in.h.dl = 0x42;
   intdos(&in, &out);
   printf("\nCharacter: %d\n", out.h.al);
 }
 
-int main()
-{
+int main(){
   test();
   return 0;
 }
